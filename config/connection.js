d5c3770323de7924f1373db21913ba47cd3372ee
Mark Edwards <109709559+mark-227-g@users.noreply.github.com>
2023-05-30 00:11:17 -0400

    added models

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,17 @@
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+ // : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize('ecommerce_db', 'root', 'BootCamp', {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
+      port: 3306
     });
 
 module.exports = sequelize;
